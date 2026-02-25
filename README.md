997fd21689880db4f08b6b6bee191448bbd32c3f
Lucas Favaro <paradigma0621@gmail.com>
2026-02-25 14:44:31 -0300

    Add basic structure of microservice 'notification'

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RABBITMQ POC #1
 
+Using Java 21 with Spring Boot 3
+
+## Microservice Orders
+## Microservice Notification
+
+
---
+++ notification/src/test/java/com/paradigma0621/notification/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigma0621.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
